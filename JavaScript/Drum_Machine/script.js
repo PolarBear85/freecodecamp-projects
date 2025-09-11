7ebdd3614656624d5f8e246a9c7fd6493f00923b
Luke Jelley <luke_jelley@hotmail.com>
2025-09-11 13:37:45 +0100

    updates to drum machine project

--- JavaScript/Drum_Machine/script.js
+++ JavaScript/Drum_Machine/script.js
@@ -1,19 +1,32 @@
-const padQ = document.querySelector("#pad-Q")
-const padW = document.querySelector("#pad-W")
-const padE = document.querySelector("#pad-E")
-const padA = document.querySelector("#pad-A")
-const padS = document.querySelector("#pad-S")
-const padD = document.querySelector("#pad-D")
-const padZ = document.querySelector("#pad-Z")
-const padX = document.querySelector("#pad-X")
-const padC = document.querySelector("#pad-C")
 
+const padDescription = document.querySelector("#display")
 const drumPads = document.querySelectorAll(".drum-pad")
+const keyToPadMap = {
+    'KeyQ' : 'pad-Q',
+    'KeyW' : 'pad-W',
+    'KeyE' : 'pad-E',
+    'KeyA' : 'pad-A',
+    'KeyS' : 'pad-S',
+    'KeyD' : 'pad-D',
+    'KeyZ' : 'pad-Z',
+    'KeyX' : 'pad-X',
+    'KeyC' : 'pad-C'
+}
 
 drumPads.forEach(pad => {
     pad.addEventListener("click",(event) => {
         event.currentTarget.querySelector('audio').play()
+        padDescription.textContent = event.currentTarget.getAttribute("label")
     })
+})
+
+document.addEventListener('keydown',(event) => {
+    const padID = keyToPadMap[event.code]
+    if (padID) {
+        const padPlay = document.querySelector(`#${padID}`)
+        padPlay.querySelector('audio').play()
+        padDescription.textContent = padPlay.getAttribute("label")
+    }
 })
 
 /*
@@ -30,5 +43,15 @@ padQ.addEventListener("click",playSound)
 padQ.addEventListener("click", (event) => {
     event.currentTarget.querySelector('audio').play() 
 })
+
+const padQ = document.querySelector("#pad-Q")
+const padW = document.querySelector("#pad-W")
+const padE = document.querySelector("#pad-E")
+const padA = document.querySelector("#pad-A")
+const padS = document.querySelector("#pad-S")
+const padD = document.querySelector("#pad-D")
+const padZ = document.querySelector("#pad-Z")
+const padX = document.querySelector("#pad-X")
+const padC = document.querySelector("#pad-C")
 
 */
