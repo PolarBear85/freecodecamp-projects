651f751f7390d830258a3be767f5635646eaf00d
Luke Jelley <luke_jelley@hotmail.com>
2025-09-11 08:57:16 +0100

    Added daily challenge and drum machine project

---
+++ JavaScript/DailyCodeChallenge/reverseSentence.js
@@ -0,0 +1,32 @@
+function reverseSentence(sentence) {
+
+const wsRegex = /\s{2,}/g
+
+const newSentence = sentence.replace(wsRegex," ");
+
+const sentArr = newSentence.split(" ")
+
+sentArr.reverse()
+const returnSent = sentArr.join(" ")
+
+
+
+return returnSent;
+
+}
+
+reverseSentence("world hello")
+reverseSentence("push commit git")
+reverseSentence("npm  install  sudo")
+reverseSentence("import    default   function  export")
+
+
+/*
+
+
+Given a string of words, return a new string with the words in reverse order. For example, the first word should be at the end of the returned string, and the last word should be at the beginning of the returned string.
+
+    In the given string, words can be separated by one or more spaces.
+    The returned string should only have one space between words.
+
+*/
