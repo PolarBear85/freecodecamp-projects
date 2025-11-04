1b74bb758d7711181c7fff16ff2f56e0bcbbb1d2
Luke Jelley <luke_jelley@hotmail.com>
2025-11-04 15:48:29 +0000

    updates to sort visualiser

--- JavaScript/Sorting_Visualiser/script.js
+++ JavaScript/Sorting_Visualiser/script.js
@@ -22,22 +22,38 @@ const isOrdered = (a,b) => a<=b;
 function swapElements(array,index) {
   console.log(array[index],array[index+1])
   console.log(array[index]>array[index+1])
-  if (array[index]>array[index+1]) {
+  if (!isOrdered(array[index],array[index+1])) {
     const tempHold = array[index+1]
     array[index+1] = array[index]
     array[index] = tempHold
-    console.log(array)
+    return false
   }
+
+  return array
 }
 
-function highlightCurrentEls() {
-
+function highlightCurrentEls(element,index) {
+  const children = element.querySelectorAll('span')
+  children[index].style.border = "1px dashed red"
+  children[index+1].style.border = "1px dashed red"
 }
 
 const generateBtn = document.querySelector("#generate-btn")
 const startingArray = document.querySelector("#starting-array")
+const sortBtn = document.querySelector("#sort-btn")
+
 
 generateBtn.addEventListener("click",() => {
   startingArray.innerHTML = ""
   startingArray.appendChild(fillArrContainer(generateContainer(),generateArray()))
+})
+
+
+sortBtn.addEventListener("click", () => {
+  const elementNode = [...startingArray.querySelectorAll('span')]
+  const firstArr = elementNode.map(element => {
+    return element.textContent
+  })
+
+
 })
