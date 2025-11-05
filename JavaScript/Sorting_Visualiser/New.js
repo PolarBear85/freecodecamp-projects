2a86d0c030b9549516ebcf91b915b641c24f5a73
Luke Jelley <luke_jelley@hotmail.com>
2025-11-05 17:33:00 +0000

    Solved sorter

---
+++ JavaScript/Sorting_Visualiser/New.js
@@ -0,0 +1,134 @@
+const generateElement =() => Math.floor((Math.random()*100)+1)
+
+function generateArray() {
+  const returnArr = []
+  while (returnArr.length < 5) {
+    returnArr.push(generateElement())
+  }
+  return returnArr
+}
+
+const generateContainer = () => document.createElement('div')
+
+function fillArrContainer(element,array) {
+  array.forEach(item => {
+    element.innerHTML += `<span>${item} </span>`
+  })
+  return element
+}
+
+const isOrdered = (a,b) => Number(a)<=Number(b);
+
+function swapElements(array,index) {
+  if (!isOrdered(array[index],array[index+1])) {
+    const tempHold = array[index+1]
+    array[index+1] = array[index]
+    array[index] = tempHold
+    return false
+  }
+
+  return true
+}
+
+function highlightCurrentEls(element,index) {
+  const children = element.querySelectorAll('span')
+  children[index].style.border = "1px dashed red"
+  children[index+1].style.border = "1px dashed red"
+}
+
+const generateBtn = document.querySelector("#generate-btn")
+const arrayContainer = document.querySelector("#array-container")
+const startingArray = document.querySelector("#starting-array")
+const sortBtn = document.querySelector("#sort-btn")
+
+
+generateBtn.addEventListener("click",() => {
+  const childrenElements = Array.from(arrayContainer.children)
+  for (let i = 0; i<childrenElements.length; i++) {
+    const child = childrenElements[i]
+    if (child.id !== startingArray.id) {
+      arrayContainer.removeChild(child)
+    }
+  }
+
+
+  startingArray.innerHTML = ""
+
+  
+  startingArray.innerHTML = `<div>
+<span>26</span>
+<span>9</span>
+<span>58</span>
+<span>23</span>
+<span>38</span>
+</div>`
+
+
+  //startingArray.appendChild(fillArrContainer(generateContainer(),generateArray()))
+})
+
+
+
+
+
+
+
+
+sortBtn.addEventListener("click", () => {
+
+  let lastChild = arrayContainer.lastElementChild;
+  const elementNode = [...lastChild.querySelectorAll('span')]
+  const currentArr = elementNode.map(element=> {
+    return parseInt(element.textContent,10)
+  })
+
+  let stillToSort = isSorted(currentArr)
+  if (stillToSort) {
+    return
+  }
+  let swapped = true
+  while (swapped) { //THIS NEEDS TO RUN 4 more times
+  swapped = false
+    for (let x = 0; x < currentArr.length-1; x++) {
+      highlightCurrentEls(lastChild,x)
+      const didSwap = swapElements(currentArr,x)
+      if (!didSwap) {
+        swapped = true
+      }
+      arrayContainer.appendChild(fillArrContainer(generateContainer(),currentArr))
+      lastChild = arrayContainer.lastElementChild
+      
+
+    } 
+
+  
+  }
+  
+
+  lastChild = arrayContainer.lastElementChild
+  lastChild.style.border ="5px solid green"
+
+  //let indexToSort = itemToSort(currentArr)
+
+
+})
+
+const sortCheck = (a,b) => Number(a)<=Number(b) ? true:false
+
+function isSorted (array) {
+  const slicedArr = array.slice(0,-1)
+  return slicedArr.every((item,index) => {
+    return sortCheck(array[index],array[index+1])
+  })
+}
+
+function itemToSort(array){
+  const slicedArr = array.slice(0,-1)
+  return slicedArr.findIndex((item,index) => {
+    return !sortCheck(array[index],array[index+1])
+  })
+  
+}
+
+
+
