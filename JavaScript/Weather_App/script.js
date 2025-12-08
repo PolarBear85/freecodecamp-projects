2a38ce8c4927043d6fa454ddb473a8cf1264f0cf
Luke Jelley <luke_jelley@hotmail.com>
2025-12-08 15:28:06 +0000

    Weather app progress

--- JavaScript/Weather_App/script.js
+++ JavaScript/Weather_App/script.js
@@ -1,17 +1,20 @@
 //Document element declarations
 const locationSelect = document.querySelector("#target")
 const getWeatherBtn = document.querySelector("#get-weather-btn")
+const weatherImg = document.querySelector("#weather-icon")
+const mainTemp = document.querySelector("#main-temperature")
+const feelsLike = document.querySelector("#feels-like")
+const humidity = document.querySelector("#humidity")
+const wind = document.querySelector("#wind")
+const windGust = document.querySelector("#wind-gust")
+const weatherMain = document.querySelector("#weather-main")
+const locationDisplay = document.querySelector("#location")
 
 
+//Event listeners
 getWeatherBtn.addEventListener("click", () => {
-    console.log("click")
-    if (locationSelect.value != "") {
-        return weatherData(locationSelect.value)
-    } else {
-        console.log("no city")
-        return
-    }
-    
+    console.log("button clicked")
+    showWeather()
 })
 
 
@@ -19,8 +22,28 @@ getWeatherBtn.addEventListener("click", () => {
 
 
 //functions
-
-const weatherData = location => fetch(`https://weather-proxy.freecodecamp.rocks/api/city/${location}`)
+const getWeather = async location => fetch(`https://weather-proxy.freecodecamp.rocks/api/city/${location}`)
     .then(response=>response.json())
-    .then(data => console.log(data))
+    .catch(error=> {
+        console.error("An error occured: ", error)
+        throw error
+    })
+
+
+async function showWeather() {
+    console.log("showWeather triggered")
+    const weatherData = locationSelect.value == "" ? "No City Selected" : await getWeather(locationSelect.value)
+    console.log(weatherData)
+    if (weatherData === undefined) {
+        console.error()
+    }
+
+    console.log(weatherData.base)
+    console.log(weatherData.name)
+
+}
+
+
+
+
 
