a08d04319b075efaa0cf1002226c9c3cf5046fcb
Luke <84390656+PolarBear85@users.noreply.github.com>
2026-01-14 08:43:43 +0000

    Complete daily challenge 26-01-14

---
+++ JavaScript/DailyCodeChallenge/260113_oddOrEven.js
@@ -0,0 +1,18 @@
+function oddOrEven(n) {
+
+  return n%2 == 0 ? "Even" :"Odd"
+
+}
+
+/*
+Odd or Even?
+Given a positive integer, return "Odd" if it's an odd number, and "Even" is it's even.
+
+Tests
+Passed:1. oddOrEven(1) should return "Odd".
+Passed:2. oddOrEven(2) should return "Even".
+Passed:3. oddOrEven(13) should return "Odd".
+Passed:4. oddOrEven(196) should return "Even".
+Passed:5. oddOrEven(123456789) should return "Odd".
+
+*/
