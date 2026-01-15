b30f4f3f9f1f6842fa87ff4c66722796f943bbd6
Luke <84390656+PolarBear85@users.noreply.github.com>
2026-01-15 08:28:18 +0000

    Complete daily challenge 26-01-15

---
+++ JavaScript/DailyCodeChallenge/260115_arraySwap.js
@@ -0,0 +1,24 @@
+function arraySwap(arr) {
+
+  return arr.reverse();
+}
+
+arraySwap([true, false])
+
+
+/*
+Array Swap
+Given an array with two values, return an array with the values swapped.
+
+For example, given ["A", "B"] return ["B", "A"].
+
+Run the Tests (Ctrl + Enter)
+Reset this lesson
+Get Help
+Tests
+Passed:1. arraySwap(["A", "B"]) should return ["B", "A"].
+Passed:2. arraySwap([25, 20]) should return [20, 25].
+Passed:3. arraySwap([true, false]) should return [false, true].
+Passed:4. arraySwap(["1", 1]) should return [1, "1"].
+
+*/
