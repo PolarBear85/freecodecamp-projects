6ae68ed9edd8b8cd835bd74b29574f6fe6f382c8
Luke <84390656+PolarBear85@users.noreply.github.com>
2026-01-20 13:18:56 +0000

    Complete daily challenge 25-08-19 in Python

---
+++ Python/Daily_Code_Challenge/250819-sum-of-squares.py
@@ -0,0 +1,18 @@
+def sum_of_squares(n):
+
+    return n*(n+1)*(2*n+1)/6 #Faulhaber's Formula
+
+'''
+Sum of Squares
+Given a positive integer up to 1,000, return the sum of all the integers squared from 1 up to the number.
+
+Run the Tests (Ctrl + Enter)
+Reset this lesson
+Get Help
+Tests
+Passed:1. sum_of_squares(5) should return 55.
+Passed:2. sum_of_squares(10) should return 385.
+Passed:3. sum_of_squares(25) should return 5525.
+Passed:4. sum_of_squares(500) should return 41791750.
+Passed:5. sum_of_squares(1000) should return 333833500.
+'''
