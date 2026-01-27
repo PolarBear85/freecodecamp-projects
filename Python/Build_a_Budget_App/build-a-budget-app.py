16952e0fb846dd03af0993bc6d0400044afa68b6
Luke <84390656+PolarBear85@users.noreply.github.com>
2026-01-27 14:27:26 +0000

    Pass tests 1-15 in Budget app

--- Python/Build_a_Budget_App/build-a-budget-app.py
+++ Python/Build_a_Budget_App/build-a-budget-app.py
@@ -1,35 +1,44 @@
 class Category:
     def __init__(self,name):
         self.ledger = []
+        self.name = name
+
+    def check_ledger(self):
+        print(self.ledger)
 
     def deposit(self, amount,description=""):
         self.ledger.append(
             {'amount':amount,
             'description':description}
         )
-        print(self.ledger)
+        print(f"{amount} deposited")
     
     def withdraw(self,amount,description=""):
-        total = self.get_balance()
-
-
-        self.ledger.append(
-            {'amount': -abs(amount),
-            'description':description}
-        )
-        
-        if amount > total:
-            return False
-        else:
-            return True
+        valid_transfer = self.check_funds(amount)
+        if valid_transfer:
+            self.deposit(-amount,description)
+        return valid_transfer
         
         
     def get_balance(self):
-        #print("in balance")
         amounts = [item['amount'] for item in self.ledger]
-        #print(amounts)
         return sum(amounts)
 
+    def transfer(self,amount,category):
+        can_transfer = self.check_funds(amount)
+        print(f"Can we transfer? {can_transfer}")
+        if can_transfer:
+            print("Start withdraw")
+            self.withdraw(amount,f"Transfer to {category.name}")
+            category.deposit(amount,f"Transfer from {self.name}")
+            return can_transfer
+        else:
+            return can_transfer
+
+        
+    def check_funds(self,amount):
+        funds = self.get_balance()
+        return funds>=amount
 
 def create_spend_chart(categories):
     pass
@@ -38,6 +47,11 @@ def create_spend_chart(categories):
 
 food = Category('Food')
 food.deposit(1000, 'deposit')
-food.withdraw(10.15, 'groceries')
-food.withdraw(15.89, 'restaurant and more food for dessert')
-food.get_balance()
+print(food.check_funds(999))
+#print(food.withdraw(100000.15, 'groceries'))
+#food.withdraw(15.89, 'restaurant and more food for dessert')
+#food.get_balance()
+clothing = Category('Clothing')
+food.transfer(50, clothing)
+clothing.check_ledger()
+food.check_ledger()
