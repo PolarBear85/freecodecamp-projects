8e5f982eda14af86300af9394b3dd2893f045bf4
Luke <84390656+PolarBear85@users.noreply.github.com>
2026-01-29 08:19:51 +0000

    Complete daily challenge 25-08-27  in Python and JavaScript

---
+++ JavaScript/DailyCodeChallenge/250827_evaluate.js
@@ -0,0 +1,43 @@
+function evaluate(numbers, operators) {
+
+  const operations = {
+    "+": (a,b) => a+b,
+    "-": (a,b)=>a-b,
+    "*": (a,b)=>a*b,
+    "/": (a,b)=>a/b,
+    "%": (a,b)=>a%b
+  }
+
+  let total = numbers[0]
+  let operator = 0
+
+  for (let x = 1; x<numbers.length; x++) {
+    const thisNum = numbers[x]
+    const thisOperator = operators[operator]
+    total = operations[thisOperator](total,thisNum)
+    operator++
+    if (operator>=operators.length) operator = 0
+  }
+
+  console.log(total)
+  return total;
+}
+
+evaluate([11, 4, 10, 17, 2], ['*', '*', '%'])
+
+/*
+Unorder of Operations
+Given an array of integers and an array of string operators, apply the operations to the numbers sequentially from left-to-right. Repeat the operations as needed until all numbers are used. Return the final result.
+
+For example, given [1, 2, 3, 4, 5] and ['+', '*'], return the result of evaluating 1 + 2 * 3 + 4 * 5 from left-to-right ignoring standard order of operations.
+
+Valid operators are +, -, *, /, and %.
+
+Tests
+Passed:1. evaluate([5, 6, 7, 8, 9], ['+', '-']) should return 3
+Passed:2. evaluate([17, 61, 40, 24, 38, 14], ['+', '%']) should return 38
+Passed:3. evaluate([20, 2, 4, 24, 12, 3], ['*', '/']) should return 60
+Passed:4. evaluate([11, 4, 10, 17, 2], ['*', '*', '%']) should return 30
+Passed:5. evaluate([33, 11, 29, 13], ['/', '-']) should return -2
+
+*/
