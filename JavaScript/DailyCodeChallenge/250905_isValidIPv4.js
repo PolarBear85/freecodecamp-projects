70d70ae94ed795747f91a2a37acc096e400fa5e2
Luke <84390656+PolarBear85@users.noreply.github.com>
2026-02-09 11:02:35 +0000

    Complete daily challenge 25-09-05  in Python and JavaScript

---
+++ JavaScript/DailyCodeChallenge/250905_isValidIPv4.js
@@ -0,0 +1,36 @@
+function isValidIPv4(ipv4) {
+
+  const regex = /^(0|[1-9][0-9]{0,2})$/
+
+  const splitIP = ipv4.split(".")
+  console.log(splitIP)
+
+  const answer = splitIP.every(item => {
+    console.log(item, regex.test(item))
+    return regex.test(item) && item <= 255
+  })
+
+  console.log(answer)
+
+  return answer;
+}
+
+isValidIPv4("255.01.50.111")
+
+/*
+IPv4 Validator
+Given a string, determine if it is a valid IPv4 Address. A valid IPv4 address consists of four integer numbers separated by dots (.). Each number must satisfy the following conditions:
+
+It is between 0 and 255 inclusive.
+It does not have leading zeros (e.g. 0 is allowed, 01 is not).
+Only numeric characters are allowed.
+
+Tests
+Passed:1. isValidIPv4("192.168.1.1") should return true.
+Passed:2. isValidIPv4("0.0.0.0") should return true.
+Passed:3. isValidIPv4("255.01.50.111") should return false.
+Passed:4. isValidIPv4("255.00.50.111") should return false.
+Passed:5. isValidIPv4("256.101.50.115") should return false.
+Passed:6. isValidIPv4("192.168.101.") should return false.
+Passed:7. isValidIPv4("192168145213") should return false.
+*/
