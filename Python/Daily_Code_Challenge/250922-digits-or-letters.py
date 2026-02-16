ad493977433dbb351ad82ce33e8fb54fc867694d
Luke <84390656+PolarBear85@users.noreply.github.com>
2026-02-16 11:00:53 +0000

    Complete daily challenge 25-09-22  in Python

---
+++ Python/Daily_Code_Challenge/250922-digits-or-letters.py
@@ -0,0 +1,30 @@
+def digits_or_letters(s):
+
+    letters = [char for char in s if char.isalpha()]
+    digits = [char for char in s if char.isdigit()]
+
+    if len(letters) > len(digits):
+        return "letters"
+    if len(letters) < len(digits):
+        return "digits"
+    return "tie"
+    
+
+digits_or_letters("H3110 W0R1D")
+
+'''
+Digits vs Letters
+Given a string, return "digits" if the string has more digits than letters, "letters" if it has more letters than digits, and "tie" if it has the same amount of digits and letters.
+
+Digits consist of 0-9.
+Letters consist of a-z in upper or lower case.
+Ignore any other characters.
+
+Tests
+Waiting:1. digits_or_letters("abc123") should return "tie".
+Waiting:2. digits_or_letters("a1b2c3d") should return "letters".
+Waiting:3. digits_or_letters("1a2b3c4") should return "digits".
+Waiting:4. digits_or_letters("abc123!@#DEF") should return "letters".
+Waiting:5. digits_or_letters("H3110 W0R1D") should return "digits".
+Waiting:6. digits_or_letters("P455W0RD") should return "tie".
+'''
