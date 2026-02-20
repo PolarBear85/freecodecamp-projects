f284b8f5ee32901cc1a9c61273713713454793fb
Luke <84390656+PolarBear85@users.noreply.github.com>
2026-02-20 09:05:10 +0000

    Daily challenge 25-10-17 in Python

---
+++ Python/Daily_Code_Challenge/251017-mask.py
@@ -0,0 +1,24 @@
+def mask(card):
+    
+    star_card = ["*" if char.isdigit() else char for char in card[:-4]]
+        
+    print("".join(star_card)+card[-4:])
+    return "".join(star_card)+card[-4:]
+
+mask("2223-0000-4845-0010")
+
+'''
+Credit Card Masker
+Given a string of credit card numbers, return a masked version of it using the following constraints:
+
+The string will contain four sets of four digits (0-9), with all sets being separated by a single space, or a single hyphen (-).
+Replace all numbers, except the last four, with an asterisk (*).
+Leave the remaining characters unchanged.
+For example, given "4012-8888-8888-1881" return "****-****-****-1881".
+
+Tests
+Waiting:1. mask("4012-8888-8888-1881") should return "****-****-****-1881".
+Waiting:2. mask("5105 1051 0510 5100") should return "**** **** **** 5100".
+Waiting:3. mask("6011 1111 1111 1117") should return "**** **** **** 1117".
+Waiting:4. mask("2223-0000-4845-0010") should return "****-****-****-0010".
+'''
