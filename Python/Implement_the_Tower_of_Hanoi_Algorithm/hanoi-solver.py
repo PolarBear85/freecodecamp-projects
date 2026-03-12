5ca109e170b4638b7627f0092cc01978f0cd492a
Luke <84390656+PolarBear85@users.noreply.github.com>
2026-03-12 16:31:58 +0000

    Hanoi solver

--- Python/Implement_the_Tower_of_Hanoi_Algorithm/hanoi-solver.py
+++ Python/Implement_the_Tower_of_Hanoi_Algorithm/hanoi-solver.py
@@ -8,6 +8,29 @@ def hanoi_solver(n):
 
     print(peg_1,peg_2,peg_3)
 
+
+
+    '''
+    move 0 - N = 3 [3,2,1][][]
+    move -1 - N = 2 [3,2,1][][]
+
+    MOVE 1 - N= 1 [3,2][][1]
+    Move 2 - N=2 [3][2][1]
+    move 3 - N= 1 [3][2,1][]
+    move 4 - N = 3 [][2,1][3]
+    move 5 - N = 1 [1][2][3]
+    move 6 - N = 2 [1][][3,2]
+    move 7 - N = 1 [][][3,2,1]
+
+
+    
+    
+    
+    
+    
+    
+    '''
+
 def toh_mover(n,src,inter,dest):
     if n == 1:
         src.pop()
