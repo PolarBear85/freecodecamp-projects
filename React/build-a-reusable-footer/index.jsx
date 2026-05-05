d890453bc72a18673c061140f8e0941f0dc64ef3
Luke <84390656+PolarBear85@users.noreply.github.com>
2026-05-05 11:14:12 +0100

    React coursework:

---
+++ React/build-a-reusable-footer/index.jsx
@@ -0,0 +1,19 @@
+export function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li><a href="#">Test 1</a></li>
+        <li><a href="#">Test 2</a></li>
+      </ul>
+      <ul>
+        <li><a href="#">Test 3</a></li>
+        <li><a href="#">Test 4</a></li>
+      </ul>
+      <ul>
+        <li><a href="#">Test 5</a></li>
+        <li><a href="#">Test 6</a></li>
+      </ul>
+      <p>This is the paragraph element {"\u00A9"}</p>
+    </footer>
+  );
+}
